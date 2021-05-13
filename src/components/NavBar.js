bf7936857d8dc0e680102fd8a7a12d8e346ba2fe
mr-chidex <mbahchiboy2@gmail.com>
2021-05-13 14:42:12 +0100

    navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../images/netflix-logo-png-2584.png";
 import "../styles/navbar.scss";
 
 const NavBAr = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShowNav(true);
+      } else {
+        setShowNav(false);
+      }
+    });
+
+    // return () => {
+    //   window.removeEventListener("scroll");
+    // };
+  }, []);
+
   return (
-    <nav>
+    <nav className={showNav && "nav-color"}>
       <div className="image-container">
         <img src={logo} alt="Logo" />
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,17 +15,20 @@ import {
 
 const Home = () => {
   return (
-    <main className="container">
-      <MovieRow url={getTrending} type="Trending Movies" />
-      <MovieRow url={getAdventure} type="Adventure Movies" />
-      <MovieRow url={getAction} type="Action Movies" />
-      <MovieRow url={getComedy} type="comedy Movies" />
-      <MovieRow url={getAnimation} type="Animation Movies" />
-      <MovieRow url={getHorror} type="Horror Movies" />
-      <MovieRow url={getMusic} type="Music Movies" />
-      <MovieRow url={getRomance} type="Romance Movies" />
-      <MovieRow url={getScienceFiction} type="Science Fiction Movies" />
-    </main>
+    <>
+      <main className="container">
+        <MovieRow url={getTrending} type="Trending Movies" />
+        <MovieRow url={getAdventure} type="Adventure Movies" />
+        <MovieRow url={getAction} type="Action Movies" />
+        <MovieRow url={getComedy} type="comedy Movies" />
+        <MovieRow url={getAnimation} type="Animation Movies" />
+        <MovieRow url={getHorror} type="Horror Movies" />
+        <MovieRow url={getMusic} type="Music Movies" />
+        <MovieRow url={getRomance} type="Romance Movies" />
+        <MovieRow url={getScienceFiction} type="Science Fiction Movies" />
+      </main>
+      <div className="ruler"></div>
+    </>
   );
 };
 
