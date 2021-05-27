a63839236d605969be12baf416beeee9882ddedb
mr-chidex <mbahchiboy2@gmail.com>
2021-05-27 11:05:27 +0100

    commented out some movie rows

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,10 +25,10 @@ const Home = () => {
         <MovieRow url={getAdventure} type="Adventure Movies" />
         <MovieRow url={getAction} type="Action Movies" />
         <MovieRow url={getComedy} type="comedy Movies" />
-        <MovieRow url={getAnimation} type="Animation Movies" />
+        {/* <MovieRow url={getAnimation} type="Animation Movies" /> */}
         <MovieRow url={getHorror} type="Horror Movies" />
-        <MovieRow url={getMusic} type="Music Movies" />
-        <MovieRow url={getRomance} type="Romance Movies" />
+        {/* <MovieRow url={getMusic} type="Music Movies" /> */}
+        {/* <MovieRow url={getRomance} type="Romance Movies" /> */}
         <MovieRow url={getScienceFiction} type="Science Fiction Movies" />
       </main>
       <div className="ruler"></div>
